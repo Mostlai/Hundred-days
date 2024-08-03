360b5556818914f08e53b02a057d8fb89c214ddc
Ask sky <2096358571@qq.com>
2024-08-03 16:18:37 +0800

    0.3.9C

--- js/bars-ui.bundle.js
+++ js/bars-ui.bundle.js
@@ -1,1 +1,63 @@
-(window.webpackJsonp_name_=window.webpackJsonp_name_||[]).push([[2],{307:function(t,a,e){"use strict";e.r(a);var s=function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("div",{staticClass:"statbars"},t._l(t.itemList,(function(a){return e("div",{key:a.key,staticClass:"hidable statbar",attrs:{"data-key":a.id}},[e("span",{staticClass:"name"},[t._v(t._s(a.name))]),e("span",{staticClass:"barspan"},[e("progbar",{class:a.id,attrs:{value:a.valueOf(),max:a.max.value,color:a.color},nativeOn:{"!mouseenter":function(e){return e.stopPropagation(),t.itemOver(e,a)}}})],1)])})),0)};s._withStripped=!0;var r=e(42),n=e(23),i=e(2),o=e(32),l={props:["bars"],mixins:[e(17).a,o.a],components:{progbar:r.a},data(){let t=n.a.getSubVars("bars");return t.hide||(t.hide={}),{hide:t.hide}},computed:{baseItems(){let t=[],a=this.bars;for(let s=a.length-1;s>=0;s--){var e=i.c.getData(a[s]);e&&t.push(e)}return t},itemList(){return this.baseItems.filter(t=>this.show(t))}}},u=e(7),c=Object(u.a)(l,s,[],!1,null,null,null);c.options.__file="src/ui/components/bars.vue";a.default=c.exports}}]);
+(window["webpackJsonp_name_"] = window["webpackJsonp_name_"] || []).push([["bars-ui"],{
+
+/***/ "./node_modules/vue-loader/lib/index.js?!./src/ui/components/bars.vue?vue&type=script&lang=js&":
+/*!***************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib??vue-loader-options!./src/ui/components/bars.vue?vue&type=script&lang=js& ***!
+  \***************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var ui_components_progbar_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ui/components/progbar.vue */ \"./src/ui/components/progbar.vue\");\n/* harmony import */ var modules_settings__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! modules/settings */ \"./src/modules/settings.js\");\n/* harmony import */ var _game__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../game */ \"./src/game.js\");\n/* harmony import */ var _uiMixin__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../uiMixin */ \"./src/ui/uiMixin.js\");\n/* harmony import */ var _itemsBase__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../itemsBase */ \"./src/ui/itemsBase.js\");\n\n\n\n\n\n\n\n\n\n/**\n * Display the progress bars listed.\n */\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n\n\tprops:[ 'bars'],\n\tmixins:[_itemsBase__WEBPACK_IMPORTED_MODULE_4__[\"default\"], _uiMixin__WEBPACK_IMPORTED_MODULE_3__[\"default\"]],\n\tcomponents:{\n\t\tprogbar:ui_components_progbar_vue__WEBPACK_IMPORTED_MODULE_0__[\"default\"]\n\t},\n\tdata(){\n\n\t\tlet ops = modules_settings__WEBPACK_IMPORTED_MODULE_1__[\"default\"].getSubVars('bars');\n\t\tif ( !ops.hide ) ops.hide = {};\n\n\t\treturn {\n\t\t\thide:ops.hide\n\t\t}\n\n\t},\n\n\tcomputed:{\n\n\t\tbaseItems(){\n\n\t\t\tlet res = [];\n\n\t\t\tlet a = this.bars;\n\t\t\tfor( let i = a.length-1; i>= 0; i-- ) {\n\n\t\t\t\tvar it = _game__WEBPACK_IMPORTED_MODULE_2__[\"default\"].getData(a[i]);\n\t\t\t\tif ( it ) res.push(it);\n\n\t\t\t}\n\n\t\t\treturn res;\n\n\t\t},\n\n\t\titemList(){\n\t\t\treturn this.baseItems.filter(v=>this.show(v))\n\t\t}\n\n\t}\n\n});\n\n\n//# sourceURL=webpack://%5Bname%5D/./src/ui/components/bars.vue?./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/ui/components/bars.vue?vue&type=template&id=1fe09137&":
+/*!*************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/ui/components/bars.vue?vue&type=template&id=1fe09137& ***!
+  \*************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(\n    \"div\",\n    { staticClass: \"statbars\" },\n    _vm._l(_vm.itemList, function(it) {\n      return _c(\n        \"div\",\n        {\n          key: it.key,\n          staticClass: \"hidable statbar\",\n          attrs: { \"data-key\": it.id }\n        },\n        [\n          _c(\"span\", { staticClass: \"name\" }, [_vm._v(_vm._s(it.name))]),\n          _c(\n            \"span\",\n            { staticClass: \"barspan\" },\n            [\n              _c(\"progbar\", {\n                class: it.id,\n                attrs: {\n                  value: it.valueOf(),\n                  max: it.max.value,\n                  color: it.color\n                },\n                nativeOn: {\n                  \"!mouseenter\": function($event) {\n                    $event.stopPropagation()\n                    return _vm.itemOver($event, it)\n                  }\n                }\n              })\n            ],\n            1\n          )\n        ]\n      )\n    }),\n    0\n  )\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack://%5Bname%5D/./src/ui/components/bars.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/ui/components/bars.vue":
+/*!************************************!*\
+  !*** ./src/ui/components/bars.vue ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _bars_vue_vue_type_template_id_1fe09137___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./bars.vue?vue&type=template&id=1fe09137& */ \"./src/ui/components/bars.vue?vue&type=template&id=1fe09137&\");\n/* harmony import */ var _bars_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./bars.vue?vue&type=script&lang=js& */ \"./src/ui/components/bars.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ \"./node_modules/vue-loader/lib/runtime/componentNormalizer.js\");\n\n\n\n\n\n/* normalize component */\n\nvar component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\n  _bars_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  _bars_vue_vue_type_template_id_1fe09137___WEBPACK_IMPORTED_MODULE_0__[\"render\"],\n  _bars_vue_vue_type_template_id_1fe09137___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"],\n  false,\n  null,\n  null,\n  null\n  \n)\n\n/* hot reload */\nif (false) { var api; }\ncomponent.options.__file = \"src/ui/components/bars.vue\"\n/* harmony default export */ __webpack_exports__[\"default\"] = (component.exports);\n\n//# sourceURL=webpack://%5Bname%5D/./src/ui/components/bars.vue?");
+
+/***/ }),
+
+/***/ "./src/ui/components/bars.vue?vue&type=script&lang=js&":
+/*!*************************************************************!*\
+  !*** ./src/ui/components/bars.vue?vue&type=script&lang=js& ***!
+  \*************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_vue_loader_lib_index_js_vue_loader_options_bars_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib??vue-loader-options!./bars.vue?vue&type=script&lang=js& */ \"./node_modules/vue-loader/lib/index.js?!./src/ui/components/bars.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__[\"default\"] = (_node_modules_vue_loader_lib_index_js_vue_loader_options_bars_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[\"default\"]); \n\n//# sourceURL=webpack://%5Bname%5D/./src/ui/components/bars.vue?");
+
+/***/ }),
+
+/***/ "./src/ui/components/bars.vue?vue&type=template&id=1fe09137&":
+/*!*******************************************************************!*\
+  !*** ./src/ui/components/bars.vue?vue&type=template&id=1fe09137& ***!
+  \*******************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_bars_vue_vue_type_template_id_1fe09137___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./bars.vue?vue&type=template&id=1fe09137& */ \"./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/ui/components/bars.vue?vue&type=template&id=1fe09137&\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_bars_vue_vue_type_template_id_1fe09137___WEBPACK_IMPORTED_MODULE_0__[\"render\"]; });\n\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_bars_vue_vue_type_template_id_1fe09137___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"]; });\n\n\n\n//# sourceURL=webpack://%5Bname%5D/./src/ui/components/bars.vue?");
+
+/***/ })
+
+}]);
